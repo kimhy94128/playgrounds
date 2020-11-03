1b89b6cfa6bd4aa75643e6b5029f59e1433d91a2
HOOK <kimhy94128@gmail.com>
2020-11-04 01:12:36 +0900

    인증구현

--- client/src/components/views/LandingPage.js
+++ client/src/components/views/LandingPage.js
@@ -1,15 +1,27 @@
-import React, { useEffect, useState } from 'react'
-// import axios from 'axios'
+import React from 'react'
+import axios from 'axios'
+import { withRouter } from 'react-router-dom'
 
-function LandingPage() {
+function LandingPage(props) {
+  const onLogoutClick = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success){
+          props.history.push('/login')
+        } else {
+          alert('로그아웃에 실패했습니다.')
+        };
+      })
+  }
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+      <button onClick={onLogoutClick}>로그아웃</button>
     </div>
   )
 }
 
-export default LandingPage
+export default withRouter(LandingPage)
