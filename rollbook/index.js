23eaf0eb439934da0c3dbd59d6f9728364bbd85f
HOOK <kimhy94128@gmail.com>
2020-11-07 13:52:40 +0900

    특정 날짜를 입력하면 해당하는 달의 1일부터 마지막 날까지 배열에 넣는 함수 생성

--- rollbook/index.js
+++ rollbook/index.js
@@ -1,10 +1,27 @@
 const now = new Date()
 const monthStart = new Date(now.setDate(1))
-const monthEnd = () => {
-  now.setMonth(now.getMonth()+1)
-  now.setDate(0)
-  return now
+// const monthEnd = () => {
+//   now.setMonth(now.getMonth()+1)
+//   now.setDate(0)
+//   return now
+// }
+
+function makeRollBook(date){
+  const days = [];
+  date = new Date(date)
+  
+  const monthStart = new Date(date.setDate(1))
+  date.setMonth(date.getMonth()+1)
+  date.setDate(0)
+  const monthEnd = date
+
+  while(monthStart.getTime() <= monthEnd.getTime()){
+    days.push(`${monthStart.getFullYear()}-${monthStart.getMonth()+1 < 10 ? `0${monthStart.getMonth()+1}` : monthStart.getMonth()+1}-${monthStart.getDate() < 10 ? `0${monthStart.getDate()}` : monthStart.getDate()}`);
+    monthStart.setDate(monthStart.getDate()+1)
+  }
+  return {
+    days
+  }
 }
 
-console.log(monthStart);
-console.log(monthEnd());
+console.log(makeRollBook('2020-03-02'));
