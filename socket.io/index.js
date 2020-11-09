d8b82fd09d78fba71c0169752a77eaf2653a1371
HOOK <HOOK@Hook-MacBook-Pro-For-DJ.local>
2020-11-09 14:42:47 +0900

    socket.io 설정

--- socket.io/index.js
+++ socket.io/index.js
@@ -1,1 +1,31 @@
-console.log('hello world!')
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+const http = require('http').createServer(app);
+const port = 3000;
+
+const io = socket(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+let onUsers = 0;
+io.on('connection', (socket) => {
+    console.log('유저가 입장했습니다.')
+    onUsers += 1;
+    console.log('현재 유저는 ', onUsers, '명')
+    socket.on('chat', (msg) => {
+        console.log('message: ', msg)
+        io.emit('chat', msg)
+    })
+    socket.on('disconnect', () => {
+        console.log('유저가 퇴장했습니다.')
+        onUsers -= 1;
+        console.log('현재 유저는 ', onUsers, '명')
+    })
+})
+
+http.listen(port, ()=>{
+    console.log(`${port}번 포트에서 서버 실행`)
+})
