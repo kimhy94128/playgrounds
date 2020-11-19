d7c1c559655a5fe4caf8a6afba282a1881223302
Yongary <kimhy94128@gmail.com>
2020-11-19 09:27:57 +0900

    1-1

--- blog-nodejs/1-1/index.js
+++ blog-nodejs/1-1/index.js
@@ -3,7 +3,14 @@
 const http = require('http');
 
 http.createServer((req, res) => {
+
+  if(req.method === 'GET' && req.url === '/'){
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello World</h1>');
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Path is '+ req.url +'</h1>');
+  }
 }).listen(3000);
